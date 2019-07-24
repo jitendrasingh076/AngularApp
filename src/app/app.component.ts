9844d4c6f5d69799b71a08c4045aaaf63df555e5
Jitendra Kumar Singh <jksingh@ISHIR.LOCAL>
2019-07-24 17:26:18 +0530

    Add Web Config File for Azure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngularAzureApp';
+
+ filesPicked(files) {
+    for (let i = 0; i < files.length; i++) {
+        const file = files[i];
+        const path = file.webkitRelativePath.split('/');
+        console.log(path);
+    // upload file using path
+    }
+}
 }
